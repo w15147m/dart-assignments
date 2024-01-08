0c037f56466be8f6b734fe15f390935b2636e2a4
wa15147m <waseemofice@gmail.com>
2024-01-08 19:43:02 +0500

    log  in quiz

---
+++ Qu's/login.dart
@@ -0,0 +1,19 @@
+void main() {
+var email =  "mola@gmail.com"; 
+var password =  "password"; 
+  
+  if(email != "mola@gmail.com" || password != "password" ){
+  
+  if(email != "mola@gmail.com" )
+  {
+          print("! email ");
+  }
+ else{
+        print("! passord ");
+  }
+}else{
+        print("login");
+    
+  }
+  
+  }
