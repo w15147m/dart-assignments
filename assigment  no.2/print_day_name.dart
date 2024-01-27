4b98feb0d2be2c0cb8768ae3df500d8b6f6587ec
wa15147m <waseemofice@gmail.com>
2024-01-27 23:33:55 +0500

    Q.2: Create an empty list of type string called days. Use the add method to add names of 7 days and print all days.

---
+++ assigment  no.2/print_day_name.dart
@@ -0,0 +1,10 @@
+// Q.2: Create an empty list of type string called days. Use the add method to add 
+// names of 7 days and print all days.
+
+void main() {
+   List<String> day_names = [] ;
+      day_names.addAll( ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]);
+     print(day_names);
+}
+
+     
