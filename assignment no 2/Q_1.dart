6c31f83a6bbd8f0ddf038794e4661ab8646b0164
wa15147m <waseemofice@gmail.com>
2024-01-28 17:32:54 +0500

    Q.1: Create a list of names and print all names using list.

---
+++ assignment no 2/Q_1.dart
@@ -0,0 +1,23 @@
+
+void main(){
+//   Q.1: Create a list of names and print all names using list.
+
+     List names = ["mola" , "noor", "waseem" , "ali" , "abbas"];
+  var number;
+
+     for(number in names){
+
+       print("name:  $number");
+
+
+     }
+
+
+}
+
+
+
+
+// Console
+// It's a rectangle.
+
