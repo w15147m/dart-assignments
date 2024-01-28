f00ae2d5b2b9c2d94fbf4005906b52692b60917b
wa15147m <waseemofice@gmail.com>
2024-01-28 19:26:09 +0500

    Q.9: Given a list of integers, write a dart code that returns the maximum value from the list.

---
+++ assignment no 2/Q_9.dart
@@ -0,0 +1,18 @@
+void main() {
+// Q.9: Given a list of integers, write a dart code that returns the maximum value 
+// from the list.
+
+  List<int> numbers = [12, 5, 8, 15, 7];
+   numbers.sort();
+   print(numbers.last);
+   
+   
+
+}
+
+// out put
+        
+
+
+
+
