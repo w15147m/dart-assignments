6caadb47c83f0d134cb7dbfade3b627b38e2520d
wa15147m <waseemofice@gmail.com>
2024-02-07 20:35:44 +0500

    table 1 to 10

---
+++ C_W/table.dart
@@ -0,0 +1,13 @@
+void main() {
+
+
+  for (var i = 1; i <= 10; i++) {
+     print("table of $i");
+  for (var a = 1; a <= 10; a++) {
+      var table =  a*i;
+     print("$i X $a = $table");
+  }
+
+  }
+  
+}
