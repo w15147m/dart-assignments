c95ba6f591e6421e056d647f937a040b085a6fab
wa15147m <waseemofice@gmail.com>
2024-02-11 13:38:10 +0500

    Write a program to make such a pattern like a right angle triangle with a number which will repeat a number in a row. The pattern like :  1  22  333  4444

--- assignment no 3/Q_13.dart
+++ assignment no 3/Q_13.dart
@@ -1,32 +1,20 @@
 void main() {
-// Q.13: Implement a code that takes in a list of integers and returns a new list 
-// containing only the unique elements from the original list. The order of 
-// elements in the new list should be the same as in the original list.
+// Write a program to make such a pattern like a right angle triangle with
+// a number which will repeat a number in a row.
+// The pattern like :
+//  1
+//  22
+//  333
+//  4444
 
-
-  List<int> integersList = [
-    1,
-    12,
-    32,
-    45,
-    45,
-    55,
-    3
-  ];
-
-Set<int> uniqueInt = {};
-List<int> uniqueElements = [];
-
-for (int str in integersList) {
-  // Check if the element is already in the set
-  if (uniqueInt.add(str)) {
-  uniqueElements.add(str);
+  for (var i = 1; i < 5; i++) {
+    for (var j = 0; j < i; j++) {
+      print(i);
+    }
+    print(''); 
   }
-}
 
-   print("original list: $integersList");
-   print("uniqueElements: $uniqueElements");
- 
+
 }
 
 // out put
