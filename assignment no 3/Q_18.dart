e632c71883e0b0b70b1b98d8a3235a722b5de4b7
wa15147m <waseemofice@gmail.com>
2024-02-11 16:05:41 +0500

    Q.18: Create a map named "person" with the following key-value pairs: "name" as "John", "age" as 25, "isStudent" as true. Write a Dart code to check if the person is both a student and over 18 years old. Print "Eligible" if both conditions are true, otherwise print "Not eligible".

--- assignment no 3/Q_18.dart
+++ assignment no 3/Q_18.dart
@@ -1,41 +1,35 @@
-void main() {
+import 'dart:io';
 
-// Q.18: Create a map named "person" with the following key-value pairs: "name" 
-// as "John", "age" as 25, "isStudent" as true. Write a Dart code to check if the 
-// person is both a student and over 18 years old. Print "Eligible" if both 
+void main() {
+// Q.18: Create a map named "person" with the following key-value pairs: "name"
+// as "John", "age" as 25, "isStudent" as true. Write a Dart code to check if the
+// person is both a student and over 18 years old. Print "Eligible" if both
 // conditions are true, otherwise print "Not eligible".
 
-
-   // Creating a map named "person"
-  Map<String, dynamic> person = {
-    "name": "John",
-    "age": 25,
-    "isStudent": true,
-  };
-
-  // Printing the "person" map
-  print("Person Map: $person");
-
-
- String personMap = checkEligible(person);
-
-  print("Person is: $personMap");
-
-
-}
-
-checkEligible(numbers) {
-
-    if(18 > numbers['age']){
-       return "NOt Eligible";
-    }else{
-       return "Eligible";
-
+  // Creating a map named "person"
+  List user_credentials = [
+    ["user1@example.com", "password1"],
+    ["user2@example.com", "password2"],
+    ["user3@example.com", "password3"],
+    ["user5@example.com", "password5"],
+  ];
+  bool isLoggedIn = false;
+  while (!isLoggedIn) {
+    stdout.write("Enter your email: ");
+    String userEmail = stdin.readLineSync()!;
+    stdout.write("Enter your password: ");
+    String userPassword = stdin.readLineSync()!;
+  for (var i = 0; i < user_credentials.length-1; i++) {
+  if (userEmail == user_credentials[i][0] && userPassword == user_credentials[i][1]) {
+      print("User login successful.");
+      isLoggedIn = true;
+      break;
+    } 
     }
-  
+ if (!isLoggedIn) {
+      print("Invalid credentials. Try again.");
+ }
+
+ 
+  }
 }
-// out put
-
-// Person Map: {name: John, age: 25, isStudent: true}
-
-// Person is: Eligible
