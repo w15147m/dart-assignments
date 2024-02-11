366a0a7075de62a32695b1eeea0fb95f3747abe2
wa15147m <waseemofice@gmail.com>
2024-02-11 13:29:06 +0500

    Q.8: Write a program to display a pattern like a right angle triangle using an asterisk using loop.

---
+++ assignment no 3/Q_11.dart
@@ -0,0 +1,23 @@
+void main() {
+// Q.8: Write a program to display a pattern like a right angle triangle using an
+// asterisk using loop.
+
+  for (var i = 1; i <= 5; i++) {
+    for (var j = 1; j <= i; j++) {
+      print('*');
+    }
+    print(''); 
+  }
+  
+}
+
+// out put
+            // *
+            // **
+            // ***
+            // ****
+
+
+
+
+
