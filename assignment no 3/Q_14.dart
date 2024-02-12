f8dcda7d22789c8817ce3e502024f99663791fe2
wa15147m <waseemofice@gmail.com>
2024-02-12 15:55:05 +0500

    Q.13:Write a program to make such a pattern like a right angle triangle with the number increased by 1 using loop.. The pattern like :  1  2 3  4 5 6  7 8 9 10

--- assignment no 3/Q_14.dart
+++ assignment no 3/Q_14.dart
@@ -1,15 +1,17 @@
 void main() {
 // Q.13:Write a program to make such a pattern like a right angle triangle with
-// a number which will repeat a number in a row.
+// the number increased by 1 using loop..
 // The pattern like :
 //  1
-//  22
-//  333
-//  4444
-  for (var i = 0; i <= 5; i++) {
+//  2 3
+//  4 5 6
+//  7 8 9 10
+var num = 0;
+  for (var i = 0; i < 5; i++) {
     String output = "";
     for (var j = 0; j < i; j++) {
-      output += "$i "; 
+      num++;
+      output += "$num "; 
     }
     print(output); 
   }
